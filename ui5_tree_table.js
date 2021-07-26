d4cf35f6323b0b812d7c0241ee3882006e448861
prantl81 <33022108+prantl81@users.noreply.github.com>
2021-07-26 18:38:21 +0200

    Update ui5_tree_table.js

--- ui5_tree_table.js
+++ ui5_tree_table.js
@@ -423,44 +423,44 @@
                                              { "spl": [
                                                      {"name":"Channel 1", "spl": [
                                                        {"name": "LH4 - Customer 1", "spl": [
-                                                         {"name": "MPL 1", "spl_flag": "X", },
-                                                         {"name": "MPL 2", "spl_flag": "", },
-                                                         {"name": "MPL 3", "spl_flag": "X", }
+                                                         {"name": "MPL 1", "spl_flag": "X" },
+                                                         {"name": "MPL 2", "spl_flag": ""  },
+                                                         {"name": "MPL 3", "spl_flag": "X" }
                                                        ]},
                                                        {"name": "LH4 - Customer 2", "spl": [
-                                                         {"name": "MPL 4", "spl_flag": "", },
-                                                         {"name": "MPL 5", "spl_flag": "X", }
+                                                         {"name": "MPL 4", "spl_flag": ""  },
+                                                         {"name": "MPL 5", "spl_flag": "X" }
                                                        ]},
                                                        {"name": "LH4 - Customer 3", "spl": [
-                                                         {"name": "MPL 6", "spl_flag": "X", },
-                                                         {"name": "MPL 7", "spl_flag": "X", },
-                                                         {"name": "MPL 8", "spl_flag": "X", },
-                                                         {"name": "MPL 9", "spl_flag": "X", }
+                                                         {"name": "MPL 6", "spl_flag": "X" },
+                                                         {"name": "MPL 7", "spl_flag": "X" },
+                                                         {"name": "MPL 8", "spl_flag": "X" },
+                                                         {"name": "MPL 9", "spl_flag": "X" }
                                                        ]},
                                                        {"name": "LH4 - Customer 4", "spl": [
-                                                         {"name": "MPL 10", "spl_flag": "X", },
-                                                         {"name": "MPL 11", "spl_flag": "X", }
+                                                         {"name": "MPL 10", "spl_flag": "X" },
+                                                         {"name": "MPL 11", "spl_flag": "X" }
                                                        ]}
                                                      ]},
                                                      {"name":"Channel 2", "spl": [
                                                        {"name": "LH4 - Customer 5", "spl": [
-                                                         {"name": "MPL 12", "spl_flag": "X", },
-                                                         {"name": "MPL 13", "spl_flag": "X", },
-                                                         {"name": "MPL 14", "spl_flag": "X", }
+                                                         {"name": "MPL 12", "spl_flag": "X" },
+                                                         {"name": "MPL 13", "spl_flag": "X" },
+                                                         {"name": "MPL 14", "spl_flag": "X" }
                                                        ]},
                                                        {"name": "LH4 - Customer 6", "spl": [
-                                                         {"name": "MPL 15", "spl_flag": "X", },
-                                                         {"name": "MPL 16", "spl_flag": "X", }
+                                                         {"name": "MPL 15", "spl_flag": "X" },
+                                                         {"name": "MPL 16", "spl_flag": "X" }
                                                        ]},
                                                        {"name": "LH4 - Customer 7", "spl": [
-                                                         {"name": "MPL 17", "spl_flag": "X", },
-                                                         {"name": "MPL 18", "spl_flag": "X", },
-                                                         {"name": "MPL 19", "spl_flag": "X", },
-                                                         {"name": "MPL 20", "spl_flag": "X", }
+                                                         {"name": "MPL 17", "spl_flag": "X" },
+                                                         {"name": "MPL 18", "spl_flag": "X" },
+                                                         {"name": "MPL 19", "spl_flag": "X" },
+                                                         {"name": "MPL 20", "spl_flag": "X" }
                                                        ]},
                                                        {"name": "LH4 - Customer 8", "spl": [
-                                                         {"name": "MPL 21", "spl_flag": "X", },
-                                                         {"name": "MPL 22", "spl_flag": "X", }
+                                                         {"name": "MPL 21", "spl_flag": "X" },
+                                                         {"name": "MPL 22", "spl_flag": "X" }
                                                        ]}
                                                      ]}
                                            ]};
