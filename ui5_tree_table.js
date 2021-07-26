322dcc7887c78ede95b70e34665dccb7fa527d51
prantl81 <33022108+prantl81@users.noreply.github.com>
2021-07-26 15:58:20 +0200

    Update ui5_tree_table.js

--- ui5_tree_table.js
+++ ui5_tree_table.js
@@ -21,7 +21,7 @@
               <m:Page showHeader="false" enableScrolling="false" class="sapUiContentPadding">
                   <m:content>
 
-                      <Table id="oTable" rows="{path'/' , parameters: {arrayNames:['TableData']}}" selectionMode="Single" selectionBehavior="RowOnly" visibleRowCount="15" sort="sortProductId" ariaLabelledBy="title" >
+                      <Table id="oTable" rows="{path:'/catalog/clothing', parameters: {arrayNames:['categories']}}" selectionMode="Single" selectionBehavior="RowOnly" visibleRowCount="15" sort="sortProductId" ariaLabelledBy="title" >
                           <extension>
                               <m:OverflowToolbar style="Clear">
                                   <m:Title id="title" text="Table Data" />
@@ -31,43 +31,15 @@
                           </extension>
                           <columns>
                               <Column sortProperty="Name" filterProperty="Name">
-                                  <m:Label text="Product Name" />
+                                  <m:Label text="Categories" />
                                   <template>
-                                      <m:Text text="{Name}" wrapping="false" />
+                                      <m:Text text="{name}" wrapping="false" />
                                   </template>
                               </Column>
-                              <Column sortProperty="ProductId" filterProperty="ProductId">
-                                  <m:Label text="Product Id" />
+                              <Column  sortProperty="Price" hAlign="End">
+                                  <m:Label text="Price" />
                                   <template>
-                                      <m:Label text="{ProductId}" />
-                                  </template>
-                              </Column>
-                              <Column  sortProperty="Quantity" hAlign="End">
-                                  <m:Label text="Quantity" />
-                                  <template>
-                                      <m:Label text="{Quantity}" />
-                                  </template>
-                              </Column>
-                              <Column  showSortMenuEntry="false">
-                                  <m:Label text="Delivery Date" />
-                                  <template>
-                                      <m:Label text="{DeliveryDate}" />
-                                  </template>
-                              </Column>
-                              <Column  hAlign="Center">
-                                  <m:Label text="SPL" />
-                                  <template>
-                                      <m:CheckBox selected="{ path: 'Heavy', type: 'sap.ui.model.type.String' }" />
-                                  </template>
-                              </Column>
-                              <Column  showSortMenuEntry="false" hAlign="Center">
-                                  <template>
-                                      <m:Button type="Default" text="Open Version" press="handleOpenVersionPress" />
-                                  </template>
-                              </Column>
-                              <Column  showSortMenuEntry="false" hAlign="Center">
-                                   <template>
-                                      <m:Button type="Reject" text="Delete Version" press="handleDeleteVersionPress"/>
+                                      <m:Label text="{amount}" />
                                   </template>
                               </Column>
                           </columns>
@@ -451,28 +423,139 @@
                                          that._firstConnection = 1;
                                      } else {
 
-                                           var oData = { "TableData": [
-                                                          { "Name": "SPL 1", "TableData" : [
-                                                            { "Name": "MPL XY", "TableData" : [
-                                                              { Name: 'MPL 1'  ,  ProductId : 'RB1234 ', Quantity: 1000, DeliveryDate: '18.03.2021' },
-                                                              { Name: 'MPL 2'  ,  ProductId : 'RB1235 ', Quantity: 2000, DeliveryDate: '20.12.2020' }
-                                                            ]},
-                                                            { "Name": "MPL YZ", "TableData" : [
-                                                              { Name: 'MPL 3'  ,  ProductId : 'RB1234 ', Quantity: 1000, DeliveryDate: '18.03.2021' },
-                                                              { Name: 'MPL 4'  ,  ProductId : 'RB1235 ', Quantity: 2000, DeliveryDate: '20.12.2020' }
-                                                            ]}
-                                                          ]},
-                                                          { "Name": "SPL 2", "TableData" : [
-                                                            { "Name": "MPL RS", "TableData" : [
-                                                              { Name: 'MPL 5'  ,  ProductId : 'RB1234 ', Quantity: 1000, DeliveryDate: '18.03.2021' },
-                                                              { Name: 'MPL 6'  ,  ProductId : 'RB1235 ', Quantity: 2000, DeliveryDate: '20.12.2020' }
-                                                            ]},
-                                                            { "Name": "MPL TV", "TableData" : [
-                                                              { Name: 'MPL 7'  ,  ProductId : 'RB1234 ', Quantity: 1000, DeliveryDate: '18.03.2021' },
-                                                              { Name: 'MPL 8'  ,  ProductId : 'RB1235 ', Quantity: 2000, DeliveryDate: '20.12.2020' }
-                                                            ]}
-                                                          ]}
-                                                        ]};
+                                           var oData = {
+  "catalog": {
+    "clothing": {
+      "categories": [
+        {"name": "Women", "categories": [
+          {"name":"Clothing", "categories": [
+            {"name": "Dresses", "categories": [
+              {"name": "Casual Red Dress", "amount": 16.99, "currency": "EUR", "size": "S"},
+              {"name": "Short Black Dress", "amount": 47.99, "currency": "EUR", "size": "M"},
+              {"name": "Long Blue Dinner Dress", "amount": 103.99, "currency": "USD", "size": "L"}
+            ]},
+            {"name": "Tops", "categories": [
+              {"name": "Printed Shirt", "amount": 24.99, "currency": "USD", "size": "M"},
+              {"name": "Tank Top", "amount": 14.99, "currency": "USD", "size": "S"}
+            ]},
+            {"name": "Pants", "categories": [
+              {"name": "Red Pant", "amount": 32.99, "currency": "USD", "size": "M"},
+              {"name": "Skinny Jeans", "amount": 44.99, "currency": "USD", "size": "S"},
+              {"name": "Black Jeans", "amount": 99.99, "currency": "USD", "size": "XS"},
+              {"name": "Relaxed Fit Jeans", "amount": 56.99, "currency": "USD", "size": "L"}
+            ]},
+            {"name": "Skirts", "categories": [
+              {"name": "Striped Skirt", "amount": 24.99, "currency": "USD", "size": "M"},
+              {"name": "Black Skirt", "amount": 44.99, "currency": "USD", "size": "S"}
+            ]}
+          ]},
+          {"name":"Jewelry", "categories": [
+              {"name": "Necklace", "amount": 16.99, "currency": "USD"},
+              {"name": "Bracelet", "amount": 47.99, "currency": "USD"},
+              {"name": "Gold Ring", "amount": 399.99, "currency": "USD"}
+            ]},
+          {"name":"Handbags", "categories": [
+            {"name": "Little Black Bag", "amount": 16.99, "currency": "USD", "size": "S"},
+            {"name": "Grey Shopper", "amount": 47.99, "currency": "USD", "size": "M"}
+          ]},
+          {"name":"Shoes", "categories": [
+            {"name": "Pumps", "amount": 89.99, "currency": "USD"},
+            {"name": "Sport Shoes", "amount": 47.99, "currency": "USD"},
+            {"name": "Boots", "amount": 103.99, "currency": "USD"}
+          ]}
+        ]},
+        {"name": "Men", "categories": [
+          {"name":"Clothing", "categories": [
+            {"name": "Shirts", "categories": [
+              {"name": "Black T-shirt", "amount": 9.99, "currency": "USD", "size": "XL"},
+              {"name": "Polo T-shirt", "amount": 47.99, "currency": "USD", "size": "M"},
+              {"name": "White Shirt", "amount": 103.99, "currency": "USD", "size": "L"}
+            ]},
+            {"name": "Pants", "categories": [
+              {"name": "Blue Jeans", "amount": 78.99, "currency": "USD", "size": "M"},
+              {"name": "Stretch Pant", "amount": 54.99, "currency": "USD", "size": "S"}
+            ]},
+            {"name": "Shorts", "categories": [
+              {"name": "Trouser Short", "amount": 62.99, "currency": "USD", "size": "M"},
+              {"name": "Slim Short", "amount": 44.99, "currency": "USD", "size": "S"}
+            ]}
+          ]},
+          {"name":"Accessories", "categories": [
+            {"name": "Tie", "amount": 36.99, "currency": "USD"},
+            {"name": "Wallet", "amount": 47.99, "currency": "USD"},
+            {"name": "Sunglasses", "amount": 199.99, "currency": "USD"}
+          ]},
+          {"name":"Shoes", "categories": [
+            {"name": "Fashion Sneaker", "amount": 89.99, "currency": "USD"},
+            {"name": "Sport Shoe", "amount": 47.99, "currency": "USD"},
+            {"name": "Boots", "amount": 103.99, "currency": "USD"}
+          ]}
+        ]},
+          {"name": "Girls", "categories": [
+            {"name":"Clothing", "categories": [
+              {"name": "Shirts", "categories": [
+                {"name": "Red T-shirt", "amount": 16.99, "currency": "USD", "size": "S"},
+                {"name": "Tunic Top", "amount": 47.99, "currency": "USD", "size": "M"},
+                {"name": "Fuzzy Sweater", "amount": 103.99, "currency": "USD", "size": "L"}
+              ]},
+              {"name": "Pants", "categories": [
+                {"name": "Blue Jeans", "amount": 24.99, "currency": "USD", "size": "M"},
+                {"name": "Red Pant", "amount": 54.99, "currency": "USD", "size": "S"}
+              ]},
+              {"name": "Shorts", "categories": [
+                {"name": "Jeans Short", "amount": 32.99, "currency": "USD", "size": "M"},
+                {"name": "Sport Short", "amount": 14.99, "currency": "USD", "size": "S"}
+              ]}
+            ]},
+            {"name":"Accessories", "categories": [
+              {"name": "Necklace", "amount": 26.99, "currency": "USD"},
+              {"name": "Gloves", "amount": 7.99, "currency": "USD"},
+              {"name": "Beanie", "amount": 12.99, "currency": "USD"}
+            ]},
+            {"name":"Shoes", "categories": [
+              {"name": "Sport Shoes", "amount": 39.99, "currency": "USD"},
+              {"name": "Boots", "amount": 87.99, "currency": "USD"},
+              {"name": "Sandals", "amount": 63.99, "currency": "USD"}
+            ]}
+          ]},
+            {"name": "Boys", "categories": [
+              {"name":"Clothing", "categories": [
+                {"name": "Shirts", "categories": [
+                  {"name": "Black T-shirt with Print", "amount": 16.99, "currency": "USD", "size": "S"},
+                  {"name": "Blue Shirt", "amount": 47.99, "currency": "USD", "size": "M"},
+                  {"name": "Yellow Sweater", "amount": 63.99, "currency": "USD", "size": "L"}
+                ]},
+                {"name": "Pants", "categories": [
+                  {"name": "Blue Jeans", "amount": 44.99, "currency": "USD", "size": "M"},
+                  {"name": "Brown Pant", "amount": 89.99, "currency": "USD", "size": "S"}
+                ]},
+                {"name": "Shorts", "categories": [
+                  {"name": "Sport Short", "amount": 32.99, "currency": "USD", "size": "M"},
+                  {"name": "Jeans Short", "amount": 99.99, "currency": "USD", "size": "XS"},
+                  {"name": "Black Short", "amount": 56.99, "currency": "USD", "size": "L"}
+                ]}
+              ]},
+              {"name":"Accessories", "categories": [
+                {"name": "Sunglasses", "amount": 36.99, "currency": "USD"},
+                {"name": "Beanie", "amount": 17.99, "currency": "USD"},
+                {"name": "Scarf", "amount": 15.99, "currency": "USD"}
+              ]},
+              {"name":"Shoes", "categories": [
+                {"name": "Sneaker", "amount": 89.99, "currency": "USD"},
+                {"name": "Sport Shoe", "amount": 47.99, "currency": "USD"},
+                {"name": "Boots", "amount": 103.99, "currency": "USD"}
+              ]}
+            ]}
+        ]}
+
+    },
+  "sizes": [
+    {"key": "XS", "value": "Extra Small"},
+    {"key": "S", "value": "Small"},
+    {"key": "M", "value": "Medium"},
+    {"key": "L", "value": "Large"}
+  ]
+  };
 
 
                                            var oModel = new JSONModel(oData);
