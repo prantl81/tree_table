7dec0e2ebdafd75eacfd30d19756b5cc6a0ff31f
prantl81 <33022108+prantl81@users.noreply.github.com>
2021-07-27 09:20:21 +0200

    Update ui5_tree_table.js

--- ui5_tree_table.js
+++ ui5_tree_table.js
@@ -16,7 +16,7 @@
            <mvc:View xmlns:mvc="sap.ui.core.mvc" xmlns="sap.ui.table" xmlns:core="sap.ui.core" xmlns:dnd="sap.ui.core.dnd" xmlns:m="sap.m" xmlns:u="sap.ui.unified" controllerName="myView.Template" height="100%">
               <m:Page showHeader="false" enableScrolling="false">
                  <m:content>
-                    <TreeTable id="TreeTable" rows="{/spl}" selectionMode="None" visibleRowCount="25" enableSelectAll="false" ariaLabelledBy="title">
+                    <TreeTable id="TreeTable" items="{path: '/spl' parameters: { $count:true}}" selectionMode="None" visibleRowCount="25" enableSelectAll="false" ariaLabelledBy="title">
                        <extension>
                           <m:OverflowToolbar style="Clear">
                              <m:SearchField id="searchField" width="30%" placeholder="{i18n>searchFieldPlaceholder}" search=".onSearch"/>
