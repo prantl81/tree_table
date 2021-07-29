e8fa2f32f638afbc6a382fea135fed671e24b9bc
prantl81 <33022108+prantl81@users.noreply.github.com>
2021-07-29 16:59:15 +0200

    Update ui5_tree_table.js

--- ui5_tree_table.js
+++ ui5_tree_table.js
@@ -348,7 +348,7 @@
               }
           } else {
             // new top node -> create all three levels
-            let mpl = {"name": mplNewRow, "spl_flag": splFlagBoolean };
+            let mpl = [{"name": mplNewRow, "spl_flag": splFlagBoolean }];
             let lh4 = {"name": lh4NewRow, "spl": mpl};
             let channel = {"name": channelNewRow, "spl": lh4};
             tableData.push(channel);
